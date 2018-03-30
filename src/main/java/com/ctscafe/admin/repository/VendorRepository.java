1bba91bf9263d0b46407a3e1ec91458eff2b55ab
mgmtcafe <36656043+mgmtcafe@users.noreply.github.com>
2018-03-30 15:12:53 +0530

    Update VendorRepository.java

--- src/main/java/com/ctscafe/admin/repository/VendorRepository.java
+++ src/main/java/com/ctscafe/admin/repository/VendorRepository.java
@@ -3,9 +3,11 @@ package com.ctscafe.admin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.ctscafe.admin.model.Vendor;
 
+@Repository
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
 	// Duplicate email
 	@Query("SELECT v.vendorId FROM Vendor v where v.vendorEmail = :email")
