4dfcf87e204fc77ded1f04a04f9679fa3e320fb1
mgmtcafe <36656043+mgmtcafe@users.noreply.github.com>
2018-03-30 15:02:03 +0530

    Update AdminApp.java

--- src/main/java/com/ctscafe/admin/AdminApp.java
+++ src/main/java/com/ctscafe/admin/AdminApp.java
@@ -2,10 +2,12 @@ package com.ctscafe.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableJpaRepositories("com.ctscafe.admin.repository")
 public class AdminApp {
 
 	public static void main(String[] args) {
