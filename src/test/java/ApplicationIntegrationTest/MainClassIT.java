74539c9aaa242fc1867ee92ffa1bf6f4b8db39e2
Krishanu Das <Krishanu.Das2@cognizant.com>
2018-03-31 18:08:03 +0530

    added adminapp test

---
+++ src/test/java/ApplicationIntegrationTest/MainClassIT.java
@@ -0,0 +1,18 @@
+package ApplicationIntegrationTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ctscafe.admin.AdminApp;
+
+@RunWith(SpringRunner.class)
+public class MainClassIT {
+
+// Test class added ONLY to cover main() invocation not covered by application tests.
+
+
+   @Test
+   public void main() {
+      AdminApp.main(new String[] {});
+   }
+}
