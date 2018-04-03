50182161b20c98c57386ad434aea5e6f91c5dbd9
Ronjoy Barthakur <mgmtcafe@gmail.com>
2018-04-03 14:10:25 +0530

    created vendor and cors filter

--- src/main/java/com/ctscafe/admin/AdminApp.java
+++ src/main/java/com/ctscafe/admin/AdminApp.java
@@ -4,16 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableJpaRepositories("com.ctscafe.admin.repository")
-@CrossOrigin
 public class AdminApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApp.class, args);
 
 	}
+
 }
