e55523544600435d030133aa1aab998408a034f6
mgmtcafe <36656043+mgmtcafe@users.noreply.github.com>
2018-04-03 16:53:35 +0530

    Update PasswordGenerator.java

--- src/main/java/com/ctscafe/admin/utilities/PasswordGenerator.java
+++ src/main/java/com/ctscafe/admin/utilities/PasswordGenerator.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class PasswordGenerator {
 	
 	public static String generatePassword() {
-		String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()\\|\\,<.>?";
+		String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 		String pwd = RandomStringUtils.random( 8, characters );
 		return pwd;
 	}
